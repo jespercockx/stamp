5dde82103a77be962c17496618fc0997aebd4b8e
Thomas Winant <thomas.winant@cs.kuleuven.be>
2015-07-29 16:55:40 +0200

    Let it compile again

--- test-core-plugin/Main.hs
+++ test-core-plugin/Main.hs
@@ -8,9 +8,9 @@ agda = error "Compiler plugin did not run correctly"
 
 main :: IO ()
 main = do
-  putStrLn "Hello World"
+  -- putStrLn "Hello World"
   -- putStrLn $ show $ Pair True 'x' `eqPair` Pair True 'x'
-  -- putStrLn $ "Barry `eqFoo` Barry: " ++ show (Barry `eqFoo` Barry)
+  putStrLn $ "Barry `eqFoo` Barry: " ++ show (Barry `eqFoo` Barry)
   -- putStrLn $ "Barry `eqFoo` Bar True: " ++ show (Barry `eqFoo` Bar True)
   -- putStrLn $ "Bar True `eqFoo` Bar True: " ++ show (Bar True `eqFoo` Bar True)
   -- putStrLn $ "Bar False `eqFoo` Bar True: " ++ show (Bar False `eqFoo` Bar True)
@@ -36,8 +36,8 @@ main = do
 --   Bar b -> "Bar" ++ show b
 
 
--- eqFoo :: Foo -> Foo -> Bool
--- eqFoo = agda
+eqFoo :: Foo -> Foo -> Bool
+eqFoo = agda
 -- eqFoo f1 f2 = case (f1, f2) of
 --   (Barry, Barry)   -> True
 --   (Bar b1, Bar b2) -> b1 == b2
@@ -52,7 +52,7 @@ main = do
 --     Pair a2 b2 -> a1 == a2 && b1 == b2
 
 
-aBool :: Functor f => (Bool -> f Bool) -> ARecord a -> f (ARecord a)
-aBool = agda
+-- aBool :: Functor f => (Bool -> f Bool) -> ARecord a -> f (ARecord a)
+-- aBool = agda
 -- aBool f a
 --   = ((\ x_a2vu -> a {_aBool = x_a2vu}) `fmap` (f (_aBool a)))
